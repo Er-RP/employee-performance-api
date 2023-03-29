4e3d2e0fedec93119520372af9ccf0fd147a707d
Krishnakumar sivanathan <116875316+krishnakumars59809@users.noreply.github.com>
2023-03-29 20:15:29 +0530

    Get task by id

--- controllers/taskController.js
+++ controllers/taskController.js
@@ -1,6 +1,7 @@
 const TASK = require("../models/taskModel");
 const USER = require("../models/userModel");
 const PROJECT = require("../models/projectModel");
+const mongoose = require("mongoose");
 
 const CREATE = async (req, res, next) => {
   try {
@@ -38,4 +39,23 @@ const GET = async (req, res) => {
   }
 };
 
-module.exports = { CREATE };
+const GET_TASK_BY_ID = async (req, res, next) => {
+  try {
+    const id = req.params;
+    const ID = mongoose.Types.ObjectId(id);
+    const task = await TASK.findById(ID, null, {
+      populate: [
+        { path: "assignee", select: "id firstName lastName fullName" },
+        { path: "project", select: "id name" },
+        { path: "createdBy", select: "id firstName lastName fullName" },
+      ],
+    });
+    if (task) {
+      return res.status(200).json({ success: true, task });
+    }
+  } catch (err) {
+    next(err);
+  }
+};
+
+module.exports = { CREATE ,GET_TASK_BY_ID};
